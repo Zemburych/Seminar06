c15aa1d3735c4d49b66b5b894cba948fbacf07c2
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-06 13:58:32 +0300

    решели задачу 41

---
+++ zad41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.Write("Введите элементы через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int kol = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        kol++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0 - {kol}");
